eb83b37a62feb2718ed4781e0a2c47ab47bcc79a
oller.esteban <estebanoller73@gmail.com>
2024-05-19 01:07:52 -0300

    postgres add

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 import express from 'express'
+import pg from 'pg'
+import { config } from 'dotenv'
+
+config()
 
 const app = express()
-
-app.get('/', (req, res) =>{
+const pool = new pg.Pool({
+    connectionString: process.env.DB_HOST,
+    ssl: true
+})
+app.get('/', (req, res) => {
     res.send('Node con PostgresSQL')
+})
+
+app.get('/ping', async (req, res) => {
+    const result = await pool.query('SELECT NOW()')
+    return res.json(result.rows[0])
 })
 
 app.listen(8000)
